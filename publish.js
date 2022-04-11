d9d80ff220e2323250a8b9f0cb5a34c6d6a1f7ce
Cristian Cuartas <cristianhz1109@gmail.com>
2022-04-11 16:34:58 -0500

    Add ui kit to treinta pp

--- publish.js
+++ publish.js
@@ -1,5 +1,4 @@
 const { exec } = require("child_process");
-let args = process.argv.slice(1);
 
 const app = "treinta-app";
 const uikit = "treinta-ui-kit";
@@ -10,24 +9,22 @@ const executeFolder = basePath.split("/").pop();
 
 const { version } = require(`${basePath}/package.json`);
 
-console.log({ executeFolder });
-console.log({ basePath });
-console.log({ args });
-console.log({ packageVersion: version });
-
 let cmdExcute;
 switch (executeFolder) {
   case uikit:
-    console.log("command add to mfe");
-    cmdExcute = `cd ${basePath} && yalc publish && cd ../${mfe} && yalc add @30SAS/${package_uikit}@${version} --dev`;
     try {
+      console.log(`command add ${package_uikit} to ${mfe}`);
+      cmdExcute = `cd ${basePath} && yalc publish && cd ../${mfe} && yalc add @30SAS/${package_uikit}@${version} --dev`;
+      exec(cmdExcute);
+      console.log(`command add ${package_uikit} to ${app}`);
+      cmdExcute = `cd ../${app} && yalc add @30SAS/${package_uikit}@${version} --dev`;
       exec(cmdExcute);
     } catch (error) {
       console.log(error);
     }
     break;
   case mfe:
-    console.log("command add to treinta-app");
+    console.log(`command add ${mfe} to ${app}`);
     cmdExcute = `cd ${basePath} && yalc publish && cd ../${app} && yalc add @30SAS/${executeFolder}@${version}`;
     try {
       exec(cmdExcute);
@@ -36,5 +33,6 @@ switch (executeFolder) {
     }
     break;
   default:
+    console.log(`Is ${app}`);
     break;
 }
